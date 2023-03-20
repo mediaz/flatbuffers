78c28d1a10fcbd44fe286ddea71717b378ef9476
atkurtul <atil.kurtulmus@zerodensity.tv>
2023-03-20 16:57:52 +0300

    200 iq enum naming

--- include/flatbuffers/idl.h
+++ include/flatbuffers/idl.h
@@ -626,7 +626,11 @@ inline bool operator<(const IncludedFile &a, const IncludedFile &b) {
 struct IDLOptions {
   // field case style options for C++
   enum CaseStyle { CaseStyle_Unchanged = 0, CaseStyle_Upper, CaseStyle_Lower };
+  #ifdef ERROR
+  #undef ERROR
+  #endif
   enum class ProtoIdGapAction { NO_OP, WARNING, ERROR };
+
   bool gen_jvmstatic;
   // Use flexbuffers instead for binary and text generation
   bool use_flexbuffers;
