9db5df1ede4135207dc841d1eba944578220d270
Furkan Can Turan <furkancan.turan@nodos.dev>
2025-06-19 17:21:48 +0300

    If default value exists in schema, add it to schema json

--- src/idl_gen_json_schema.cpp
+++ src/idl_gen_json_schema.cpp
@@ -340,7 +340,20 @@ class JsonSchemaGenerator : public BaseGenerator {
         }
         std::string typeLine = Indent(4) + "\"" + property->name + "\"";
         typeLine += " : {" + NewLine() + Indent(8);
-        typeLine += GenType(property->value.type);
+        if (property->value.constant == "0") {
+          typeLine += GenType(property->value.type);
+        } else {
+          if (property->value.type.enum_def) {
+            auto value = property->value.type.enum_def->FindByValue(
+                property->value.constant);
+            typeLine +=
+                "\"type\": \"string\", \"enum\": \"" + value->name +
+                "\"";
+          } else {
+            typeLine += GenType(property->value.type.base_type) +
+                        ", \"default\": " + property->value.constant;
+          }
+        }
         typeLine += arrayInfo;
         typeLine += deprecated_info;
 #if defined(NOS_CUSTOM_FLATBUFFERS) && NOS_CUSTOM_FLATBUFFERS
